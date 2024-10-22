5ecad809c6e06307b85ceaa821d3234067aa6ee8
Christian Muise <christian.muise@gmail.com>
2024-10-22 10:51:20 -0400

    Gearing up for having 4 versions (rotation copies) of a single tile

---
+++ utils.py
@@ -0,0 +1,9 @@
+
+def rotate_tile_multiple(_edges, n):
+    edges = _edges[:]
+    for _ in range(n):
+        edges = rotate_tile(edges)
+    return edges
+
+def rotate_tile(edges):
+    return [(((x+1) % 8)+1, ((y+1) % 8)+1) for (x,y) in edges]
