969f7d01b9080ef6dac53fd04aae7eb880a0edf8
Christian Muise <christian.muise@gmail.com>
2024-10-22 11:15:48 -0400

    Quick solution printing.

--- utils.py
+++ utils.py
@@ -7,3 +7,9 @@ def rotate_tile_multiple(_edges, n):
 
 def rotate_tile(edges):
     return [(((x+1) % 8)+1, ((y+1) % 8)+1) for (x,y) in edges]
+
+
+def display_solution(S):
+    for k in S:
+        if S[k]:
+            print(k)
